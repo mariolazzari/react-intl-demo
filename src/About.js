c9727fc445862cbcb71b383813289b8bc44890e7
mariolazzari <mario.lazzari@gmail.com>
2018-12-04 23:01:22 +0100

    Added component

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FormattedMessage } from "react-intl";
+
+const About = () => (
+  <div>
+    <h1>
+      <FormattedMessage id="about.title" />
+    </h1>
+  </div>
+);
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -2,28 +2,42 @@ import React, { Component } from "react";
 import { IntlProvider } from "react-intl";
 import "./App.css";
 import Home from "./Home";
-import messages from "./messages";
+import About from "./About";
+
+// languages
+import it from "./locales/it.json";
+import en from "./locales/en.json";
+import es from "./locales/es.json";
+
+// messages
+const messages = {
+  it,
+  en,
+  es
+};
 
 class App extends Component {
   state = {
     lang: "en",
     locale: "en"
   };
+
+  // switch locales
   handleChangeLanguage = lang => this.setState({ lang });
 
   render() {
+    const { lang, locale } = this.state;
+
     return (
-      <IntlProvider
-        messages={messages[this.state.lang]}
-        locale={this.state.locale}
-      >
+      <IntlProvider messages={messages[lang]} locale={locale}>
         <div className="App">
           <header>
-            <button onClick={() => this.handleChangeLanguage("it")}> IT</button>
-            <button onClick={() => this.handleChangeLanguage("en")}> EN</button>
+            <button onClick={() => this.handleChangeLanguage("it")}>IT</button>
+            <button onClick={() => this.handleChangeLanguage("en")}>EN</button>
             <button onClick={() => this.handleChangeLanguage("es")}>ES</button>
           </header>
           <Home />
+          <About />
         </div>
       </IntlProvider>
     );
